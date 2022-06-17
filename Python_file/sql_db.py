aeb0e2e948a768b3555ebc86aa22b672aaaf9c26
Jaiharish-passion07 <78978975+Jaiharish-passion07@users.noreply.github.com>
2022-06-17 19:41:37 +0530

    Update sql_db.py

--- Python_file/sql_db.py
+++ Python_file/sql_db.py
@@ -7,11 +7,11 @@ def create_table(table_name):
         conn = None
 
         with psycopg2.connect(
-                database="crypto_db",
-                user='postgres',
-                password='123jai',
-                host='localhost',
-                port='5432') as conn:
+                database="#####",
+                user='#####',
+                password='#####',
+                host='#####',
+                port='#####') as conn:
             with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
                 # call a stored procedure
                 cur.execute('''CALL pr_create_tables(%s)''',(table_name,))
@@ -30,11 +30,11 @@ def insert_table(table_name,insert_values):
         conn = None
 
         with psycopg2.connect(
-                database="crypto_db",
-                user='postgres',
-                password='123jai',
-                host='localhost',
-                port='5432') as conn:
+                database="#####",
+                user='#####',
+                password='#####',
+                host='#####',
+                port='#####') as conn:
             with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
                 if table_name == "top_100_coins":
                     # Insert elements into DB
